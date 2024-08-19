93022ff836b4366f3b03ed9e35190a96034f44eb
Theus97carvalho <167145188+Theus97carvalho@users.noreply.github.com>
2024-08-19 18:00:23 -0300

    Add files via upload

---
+++ 9 fibonati - repetição/9 fibonati - repetição/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_fibonati___repetição
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int anterior = 1;
+            int atual = 1;
+            int proximo;
+            int i;
+            Console.WriteLine("Os 30 primeiros valores da série de Fibonacci são:");
+
+            Console.Write($"{anterior}, {atual}");
+
+            for ( i = 3; i <= 30; i++)
+            {
+                proximo = anterior + atual;
+                Console.Write($", {proximo}");
+
+                anterior = atual;
+                atual = proximo;
+            }
+
+            Console.WriteLine();
+
+        }
+    }
+}
